e5f295625e1148ed77f61c8d461a282bcef1d50c
mfisher1411 <matthewfisher1411@gmail.com>
2025-03-04 16:17:53 +0500

    create http client and http-server

---
+++ week_1/http/cmd/http_client/main.go
@@ -0,0 +1,9 @@
+package main
+
+const (
+	baseUrl       = "http://localhost:8081"
+	createPostfix = "/notes"
+	getPostfix    = "/notes/%d"
+)
+
+func main() {}
---
+++ week_1/http/go.mod
@@ -0,0 +1,5 @@
+module github.com/mfisher1411/microservices_course/week_1/http
+
+go 1.23.5
+
+require github.com/go-chi/chi/v5 v5.2.1
