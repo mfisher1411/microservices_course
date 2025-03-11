c03e88523a58e0c82c5c2f0fef7e7b52de7603b3
mfisher1411 <matthewfisher1411@gmail.com>
2025-03-11 16:28:40 +0500

    some fix

--- week_2/grpc/cmd/grpc_server/main.go
+++ week_2/grpc/cmd/grpc_server/main.go
@@ -28,7 +28,7 @@ func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetRespon
 		Note: &desc.Note{
 			Id: req.GetId(),
 			Info: &desc.NoteInfo{
-				Title:    "NEW_TITLE",
+				Title:    gofakeit.BeerName(),
 				Contex:   gofakeit.IPv4Address(),
 				Author:   gofakeit.Name(),
 				IsPublic: gofakeit.Bool(),
